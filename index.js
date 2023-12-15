40f171b413e6df814ac5996d8fb4ad61dd592422
Dmitry Orlov <d.orlov777@gmail.com>
2023-12-15 15:27:41 +0300

    fix: cookie on localhost

--- index.js
+++ index.js
@@ -621,6 +621,25 @@ function hash(sess) {
  */
 
 function issecure(req, trustProxy) {
+  // socket is localhost
+  if (
+    req.connection.remoteAddress === "127.0.0.1" ||
+    req.connection.remoteAddress === "::ffff:127.0.0.1" ||
+    req.connection.remoteAddress === "::1"
+  ) {
+    // if proxy is trusted; localhost connection is secure for sure
+    if (trustProxy === true) {
+      return true;
+    }
+
+    // proxy not explicitly trusted; no proxy means connection is secure
+    if (req.headers["x-forwarded-proto"] !== undefined) {
+      return true;
+    }
+
+    // proxy connected from localhost, we need to do other checks
+  }
+
   // socket is https server
   if (req.connection && req.connection.encrypted) {
     return true;
@@ -639,9 +658,10 @@ function issecure(req, trustProxy) {
   // read the proto from x-forwarded-proto header
   var header = req.headers['x-forwarded-proto'] || '';
   var index = header.indexOf(',');
-  var proto = index !== -1
-    ? header.substr(0, index).toLowerCase().trim()
-    : header.toLowerCase().trim()
+  var proto =
+    index !== -1
+      ? header.substr(0, index).toLowerCase().trim()
+      : header.toLowerCase().trim()
 
   return proto === 'https';
 }
